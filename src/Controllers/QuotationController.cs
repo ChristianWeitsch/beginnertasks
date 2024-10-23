99eb9e33864632376f39354bebc5c4e298042434
k.ditschke <l.ditschke@schenckprocess.com>
2024-10-23 13:52:54 +0200

    - Refactor QuotationController - Add Quote class

--- src/Controllers/QuotationController.cs
+++ src/Controllers/QuotationController.cs
@@ -1,29 +1,23 @@
-using System.Security.Cryptography;
+using BeginnerTasks.Models;
 using Microsoft.AspNetCore.Mvc;
 
-
-namespace QuotationRueckgabe.Controllers
+namespace BeginnerTasks.Controllers
 {
     [ApiController]
-    [Route("Aufgabe1/[controller]")]
-    public class QuotationRueckgabe : ControllerBase
+    [Route("task1/[controller]")]
+    public class QuotationController : ControllerBase
     {
-
-        private string a = "Ich bin der erste Quote, ";
-        private string b = "während ich der zweite Quote bin.";
-
-        string[] QuotesArray = new string[] { "Ich bin das erste Element aus dem Array", "Ich bin das zweite Element aus dem Array", "Ich bin das dritte Element aus dem Array" };
-
 
         [HttpGet]
         public IActionResult Get()
         {
-            Random random = new Random();
-            int randomIndex = random.Next(0, QuotesArray.Length);
-            string randomQuote = QuotesArray[randomIndex];
-
-            return Ok(a + b + " " + randomQuote);
+            var text = "Look deep into nature, and then you will understand everything better.";
+            var author = "Albert Einstein";
+            var category = "Nature";
+            var quote = new Quote(1, text, author, category);
+            var secondQuote = new Quote();
+            var quoteArray = new[] { quote , secondQuote};
+            return Ok(quoteArray);
         }
-
     }
 }
